fd029edb86fb00886c841374eae9ed8178ed6fff
strickvl <stricksubscriptions@gmail.com>
2016-10-09 15:19:03 +0300

    add sql code for player ranking table
    code is currently functional but missing zeroes

--- tournament.sql
+++ tournament.sql
@@ -25,4 +25,5 @@ create table matches (id serial primary key, player1 integer references players,
 -- THIS AREA RESERVED FOR CREATING VIEWS
 
 create view listofwins as select matches.winner, count(matches.id) as numberofwins from matches group by matches.winner order by numberofwins desc;
-create view formalrankingtable as select players.id, players.name, listofwins.numberofwins from players left join listofwins on players.id = listofwins.winner;
+create view formalrankingtableminusmatchesplayed as select players.id, players.name, listofwins.numberofwins as numberofwins from players left join listofwins on players.id = listofwins.winner;
+create view matchesplayed as select players.id as id, count(*) as numberofmatchesplayed from players LEFT JOIN matches on players.id = matches.player1 OR players.id = matches.player2 group by players.id;
