8da4a1e57536cdb12737adc65342f50c08d5df4e
strickvl <stricksubscriptions@gmail.com>
2016-10-11 13:31:29 +0300

    amend SQL database setup and formatting

--- tournament.sql
+++ tournament.sql
@@ -10,33 +10,37 @@
 -- The template file tournament.sql is where you will put the database schema, in the form of SQL create table commands. Give your tables names that make sense to you, and give the columns descriptive names. You'll also need to create the database itself; see below.
 
 -- connects to the tournament database
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
 \c tournament
 
 -- creates the initial table of 'players' for the database
 -- populates it with a name and serial id (allocated as primary key)
 
 --drop table if exists players;
-create table players (id serial primary key, name text);
+create table players (
+	id serial primary key,
+	name text
+	);
 
 -- creates the intial blank table of 'matches' for the database
 -- populates it with a serial id (allocated as primary key) for each match
 -- and also a record of who was playing who
 
 -- drop table if exists matches;
-create table matches (id serial primary key, player1 integer references players, player2 integer references players, winner integer references players);
+create table matches (
+	id serial primary key,
+	player1 integer references players(id),
+	player2 integer references players(id), 
+	winner integer references players(id)
+	);
 
--- clears the view prior to generating, in case it already exists
-drop view if exists listofwins;
 -- creates a view specifying how many wins all the players have had
 create view listofwins as select players.id, players.name, count(matches.id) as numberofwins from players left join matches on matches.winner = players.id group by matches.winner, players.id, players.name order by numberofwins desc;
 
--- clears the view prior to generating, in case it already exists
-drop view if exists matchesplayed;
 -- creates a view specifying how many matches all the players have played
 create view matchesplayed as select players.id as id, count(*) as numberofmatchesplayed from players LEFT JOIN matches on players.id = matches.player1 OR players.id = matches.player2 group by players.id order by players.id;
 
--- clears the view prior to generating, in case it already exists
-drop view if exists finalrankingtable;
 -- creates a view combining listofwins and matches played to generate the final ranking table
 -- this fulfills the requirements for the function playerStandings
 create view finalrankingtable as select listofwins.id, listofwins.name, listofwins.numberofwins as wins, matchesplayed.numberofmatchesplayed as matches from listofwins, matchesplayed where listofwins.id = matchesplayed.id order by numberofwins DESC;
